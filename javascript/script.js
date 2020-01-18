68676d6efe7ac7d3a490de4c0e8dd69ac06d297e
Joel Scott <jscottusf@gmail.com>
2020-01-18 00:03:55 -0500

    gifs animate upon clicking, form and button added, not functional yet

--- javascript/script.js
+++ javascript/script.js
@@ -1,30 +1,25 @@
-let movies = ["silver linings playbook", "jurassic park", "Frozen", "Moana"];
+let movies = ["Silver Linings Playbook", "Jurassic park", "Frozen", "Moana", "Malcolm X", "The Princess and the Frog", "Star Wars", "Up", "The Martian", "Saving Private Ryan", "Rick and Morty", "Back to the Future", "The Lion King", "Toy Story", "Forrest Gump", "The Great Gatsby", "The Pianist", "Schindler's List", "Joker", "The Dark Knight", "Catch Me if you Can", "Titanic", "Shrek"];
+let newMovie = "";
 
 window.onload = function() {
     for (var i = 0; i < movies.length; i++) {
-        var gifButton = $('<button type="button" id="' + movies[i] + '" class="movie btn btn-dark m-1"">' + movies[i] + '</button>')
+        var gifButton = $('<button type="button" id="' + movies[i] + '" class="movie btn btn-info m-1"">' + movies[i] + '</button>')
         $("#buttons").append(gifButton);
     }
-    
+    $("#form").html('<input class="form-control-sm new-movie" type="text"  placeholder="more movies..."><button type="button" class="query_btn btn m-1 btn-info btn-default btn-sm" id="new-movie">Add movie...</button>')
 };
 
 $(document).ready(function() {
+    
     function displayGifs(data) {
         for(var j = 0; j < 10; j++) {
             let gifDiv = $('<div class="gifDiv">');
             $("#gifs").append(gifDiv);
-            let gifImgStill = $('<img id="gif still" src="' + data.data[j].images.downsized_still.url + '">');
-            let gifImg = $('<img id="gif animate" src="' + data.data[j].images.downsized_large.url + '">');
+            let gifImgStill = $('<img class="gif" src="' + data.data[j].images.fixed_height_still.url + '">');
             let gifRating = $('<p>Rating: ' + data.data[j].rating + '</p>')
             console.log("success got data", data);
-            $(gifDiv).append(gifImg, gifRating);
+            $(gifDiv).append(gifImgStill, gifRating);
         }
-        // $(gifImg).on("click", function() {
-        //     $(this).append(gifImg);
-        // });
-        // $(gifImgStill).on("click", function() {
-        //     $(this).append(gifImgStill);
-        // });
     };
     
     function searchGIFY(movie) {
@@ -45,36 +40,25 @@ $(document).ready(function() {
         var movieTitle = $(this).attr("id");
         searchGIFY(movieTitle);
     });
+
+    $("#new-movie").click(function(){
+        newMovie = $(".new-movie").val();
+        console.log(newMovie);
+        movies.push(newMovie);
+        console.log(movies);
+
+    });
+
+    $('body').on('click', '.gif', function() {
+    	var src = $(this).attr("src");
+      if($(this).hasClass('playing')){
+         //stop
+         $(this).attr('src', src.replace(/\.gif/i, "_s.gif"))
+         $(this).removeClass('playing');
+      } else {
+        //play
+        $(this).addClass('playing');
+        $(this).attr('src', src.replace(/\_s.gif/i, ".gif"))
+      }
+    });
 });
-
-// $("#gif").on("click", function() {
-//     if (gifImgStill) {
-//         $(this).html(gifImg);
-//     }
-//     else if (gifImg) {
-//         $(this).html(gifImgStill);
-//     }
-// }); 
-
-// Search the OMDB API for the following movies, and append table rows for each
-//console.log(xhr.done.data[0]);
-
-// function createRow(data) {
-//     var tRow = $("<tr>");
-//     var titleTd = $("<td>").text(data.Title);
-//     var yearTd = $("<td>").text(data.Year);
-//     var actorsTd = $("<td>").text(data.Actors);
-//     tRow.append(titleTd, yearTd, actorsTd);
-//     $("tbody").append(tRow);
-// };
-
-// function searchGIFY(movie) {
-//     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=xNLXdGgCbMsJEGAkltPSxL0LL24n3Y4N&limit=10";
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function(response) {
-//         //createRow(response);
-//         console.log(searchGIFY("saving private ryan"));
-//     });
-// };
